7f983731a3e0003d9af649a6807d9e847fea9e1c
FrancescoAutera <>
2026-01-10 11:09:20 +0100

    add explosion behaviour

---
+++ OmbraDiArutea/Assets/Scripts/Spells/ExplosionSpell.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+namespace OmbreDiAretua
+{
+    public class ExplosionSpell : MonoBehaviour
+    {
+        [SerializeField] int damageAgaintsElementalTypeStrong;
+        [SerializeField] ElementalType _elementalType;
+        [SerializeField] SfxPlayer _soundHitted;
+        [SerializeField] DamageShower _damageShower;
+        private int damage;
+        private int _playerDamage;
+
+        public void Init(int playerDamage,int spellDamage)
+        {
+            damage = spellDamage;
+            _playerDamage = playerDamage;
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.GetComponent<Enemy>())
+            {
+                Execute(other.gameObject);
+            }
+        }
+        
+        public void Execute(GameObject gameObject)
+        {
+            var enemy = gameObject.GetComponent<Enemy>();
+            if (!enemy)
+            {
+                return;
+            }
+            _soundHitted.PlayFx();
+            if (_elementalType == enemy.ElementalType)
+            {
+                damage += damageAgaintsElementalTypeStrong;
+            }
+            damage += _playerDamage;
+            enemy.TakeDamage(damage);
+            var damagerShower = Instantiate(_damageShower, enemy.transform.position, Quaternion.identity);
+            damagerShower.ShowDamage(damage);
+        }
+    }
+}
--- OmbraDiArutea/Assets/Scripts/Spells/WaterSpellBehaviour.cs
+++ OmbraDiArutea/Assets/Scripts/Spells/WaterSpellBehaviour.cs
@@ -1,9 +1,12 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace OmbreDiAretua
 {
     public class WaterSpellBehaviour : SpellBehaviour
     {
+        [SerializeField] private ExplosionSpell _explosionSpell;
+        
         public override void Execute(GameObject gameObject)
         {
             base.Execute(gameObject);
